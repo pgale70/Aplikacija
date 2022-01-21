9b33435a00f13b5468775d5860d5052380e9699c
Predrag Galic <pg1970tb@gmail.com>
2022-01-21 17:48:34 +0100

    Implement a global API response mechanism

--- src/services/administrator/administrator.service.ts
+++ src/services/administrator/administrator.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Administrator } from 'entities/administrator.entity';
+import { resolve } from 'path/posix';
 import { AddAdministratorDto } from 'src/dtos/administrator/add.administrator.dto';
 import { EditAdministratorDto } from 'src/dtos/administrator/edit.administrator.dto';
+import { ApiResponse } from 'src/misc/api.response.class';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -20,23 +22,36 @@ export class AdministratorService {
         return this.administrator.findOne(id);
     }
 
-    add(data: AddAdministratorDto) {
+    add(data: AddAdministratorDto): Promise<Administrator | ApiResponse> {
+        
         const crypto = require('crypto');
-
         const passwordHash=crypto.createHash('sha512');
         passwordHash.update(data.password);
-
         const passwordHashString = passwordHash.digest('hex').toUpperCase();
 
         let newAdmin: Administrator = new Administrator();
         newAdmin.username = data.username;
         newAdmin.passwordHash = passwordHashString;
 
-        return this.administrator.save(newAdmin);
+        return new Promise((resolve) => {
+            this.administrator.save(newAdmin)
+            .then(data => resolve(data))
+            .catch(error => {
+                const response: ApiResponse = new ApiResponse("error", -1001);
+                resolve(response);
+            });
+        });
+        
     }
 
-    async editById(id: number, data: EditAdministratorDto): Promise<Administrator> {
+    async editById(id: number, data: EditAdministratorDto): Promise<Administrator | ApiResponse> {
         let admin: Administrator = await this.administrator.findOne(id);
+
+        if(admin === undefined) {
+            return new Promise((resolve) => {
+                resolve(new ApiResponse("error", -1002));
+            });
+        }
 
         const crypto = require('crypto');
         const passwordHash=crypto.createHash('sha512');
