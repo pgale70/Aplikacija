743070b3d8e32667fcae7bd9660b738b0a9b3604
Pedja Galic <pg1970tb@example.com>
2022-01-26 11:24:17 +0100

    Implement statically served photos

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { StorageConfig } from 'config/storage.config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  //mijenjamo vrstu aplikacije poslije cretae <>
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // serviranje statički asseta za fotografije
+  app.useStaticAssets(StorageConfig.photo.destination, {
+    prefix: StorageConfig.photo.urlPrefix,
+    maxAge: StorageConfig.photo.maxAge, // 7 dana smo ostavili da slika bude u kešu
+    index: false,  // ne možemo listati sadržaj direktorijuma u kome su slike
+  })
   await app.listen(3000);
 }
 bootstrap();
